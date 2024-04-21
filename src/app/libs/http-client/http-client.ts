b7221d20a0609af28b7d62bea86b0bd5405b4982
changhwan hwang <ch.hwang@ecubelabs.com>
2024-04-21 19:02:37 +0900

    fix: stupid config'

--- src/app/libs/http-client/http-client.ts
+++ src/app/libs/http-client/http-client.ts
@@ -2,14 +2,17 @@ import Axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 import { API_ENDPOINT } from "@configs";
 
 export const httpClient = (() => {
-  const axios = Axios.create({ baseURL: API_ENDPOINT, withCredentials: true });
+  const axios = Axios.create({
+    baseURL: API_ENDPOINT,
+    withCredentials: true,
+    headers: { "Content-Type": "application/json; charset=utf-8" },
+  });
   axios.interceptors.request.use((config) => {
     if (!config?.headers) {
       throw new Error(
         `Expected 'config' and 'config.headers' not to be undefined`
       );
     }
-    config.headers["Content-Type"] = "application/json; charset=utf-8";
     config.headers["X-Requested-With"] = "XMLHttpRequest";
     config.headers.Accept = "*/*";
     return config;
